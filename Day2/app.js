7aaae2de4fd0b3633721d1a48809a51385ae18ba
AJAY SHARMA <2015ajays@gmail.com>
2018-01-18 22:46:01 +0530

    Aangular Binding Directives {{}}
    they can move text anywhere .we need into the HTML.
    <h1>{{person.fname}}</h1>
    <h2>{{person.lname}}</h2>

---
+++ Day2/app.js
@@ -0,0 +1,17 @@
+
+var MainCtrl = function($scope){
+ /*  var person = {
+    fname = "AJAY",
+    lname = "Sharma",
+   };
+*/
+    $scope.message = "hello ANGULAR";
+    var person = {
+        fname : "AJAY",
+        imgSrc : "AJY.png",
+        lname : "Sharma"
+        
+    };
+    $scope.person = person;
+
+};
