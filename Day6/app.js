e5579ea483fb5fe5ad9865b77401a5ec4d68f75d
AJAY SHARMA <2015ajays@gmail.com>
2018-01-26 18:25:32 +0530

    NG-CLICK, Filter, ng-show, ng-hide  to form submit github api fetched data

---
+++ Day6/app.js
@@ -0,0 +1,39 @@
+(function() {   /*IIFE FUNCTION*/
+var app = angular.module("githubviewer",[]);
+    
+ 
+var MainCtrl = function($scope,$http){
+    var OnUserComplete = function(response){   /*return when data come succefully from api*/
+        $scope.user = response.data;           /*store response data in user scope*/
+            
+        $http.get($scope.user.repos_url)  /*return promise for repositary*/ 
+        .then(onRepos,on_errors_repos);        
+    };
+    
+        var onRepos = function(response){
+            $scope.repos = response.data;   /*on succesive get data of repositary*/
+        };
+        
+        var on_errors_repos = function(reason){
+            $scope.error = "user repositary data could not fetched.."
+        };
+
+        var on_errors = function(reason){
+            $scope.error = "user data could not fetched ...";  
+        };
+  
+
+    $scope.search = function(username){
+        $http.get("https://api.github.com/users/" + username)    
+        .then(OnUserComplete,on_errors);
+
+    };
+
+
+    $scope.username = "angular";  
+    $scope.message = "Github User";
+    $scope.reposortOrder = '-stargazers_count';  /*filter for repos according to stargazer going value in  reposortOrder*/
+}; 
+/*app.controller("MainCtrl",["$scope","$http",MainCtrl]);    you can write this also to minifi code*/
+app.controller("MainCtrl",MainCtrl);   
+}());
